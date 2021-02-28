c5f21347b5f831be1007bcf9a18963bcaec7d34d
pikaninja <mileshua06@gmail.com>
2021-02-27 18:01:50 -0800

    rename to commands

--- example.py
+++ example.py
@@ -1,7 +1,7 @@
-import client
-bot = client.Command(prefix = "!")
+import commands
+bot = commands.Client(prefix = "!")
 
-@bot.command(name = "ping", checks = [lambda m: m.guild], cooldown = client.Cooldown(1, 5, "author"), help = "hi")
+@bot.command(name = "ping", checks = [lambda m: m.guild], cooldown = commands.Cooldown(1, 5, "author"), help = "hi")
 async def pong(message, arg, arg2):
     await message.channel.send(f"hai {arg} | {arg2}")
 
